0245cb92917ebad0fefd955447fe18a1413b8b5d
WildCatFish <heqianyao@gmail.com>
2021-08-13 11:15:20 +0800

    1.0
    multiple tickers

--- src/main/java/com/example/app1/stock/StockRepository.java
+++ src/main/java/com/example/app1/stock/StockRepository.java
@@ -2,10 +2,12 @@ package com.example.app1.stock;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface StockRepository extends JpaRepository<Stock, StockPK> {
